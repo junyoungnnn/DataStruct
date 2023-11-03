bde8b7be6e761c30dd064c0b385aa950b85d6f65
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2023-11-03 19:48:31 +0900

    (2023.11.03) 힙 삭제

--- DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct.cpp
@@ -196,17 +196,47 @@ int main()
 
 #pragma region 힙
 
-HEAP<int> heap;
-heap.Insert(13);
-heap.Insert(15);
-heap.Insert(20);
-heap.Insert(12);
-heap.Insert(30);
-heap.Insert(50);
-heap.Insert(45);
-heap.Display();
+//HEAP<int> heap;
+//heap.Insert(13);
+//heap.Insert(15);
+//heap.Insert(20);
+//heap.Insert(12);
+//heap.Insert(30);
+//heap.Insert(50);
+//heap.Insert(45);
+//cout <<"최댓값: " <<  heap.Delete() << endl;
+//cout <<"최댓값: " <<  heap.Delete() << endl;
+//heap.Display();
 
 #pragma endregion
+
+#pragma region 해시 테이블
+    // (Key, Value)로 데이터를 저장하는 자료 구조 중 하나로
+    // 빠르게 데이터를 검색할 수 있는 자료 구조입니다.
+
+    // 해시 테이블의 평균 시간 복잡도는 O(1)입니다.
+
+    // 해시 충돌을 해결하는 방법
+    // <체이닝 기법>
+    // 각 해시 버킷을 연결 리스트로 구성하는 방식입니다.
+
+    // 해시 충돌 발생 시 동일한 해시 값에 해당하는 데이터들을
+    // 연결리스트로 연결하여 저장합니다.
+    
+    // <개방 주소법>
+    // 충돌 발생 시 빈 버킷에 데이터를 저장하는 방식입니다.
+    // 빈 버킷을 어떻게 결정할 지에 따라 구현 방식이 달라집니다.
+
+    // 선형 탐사 : 충돌 발생 시 앞에서 부터 차례대로 빈 버킷을 찾아
+    // 값을 저장하는 방식입니다.
+    
+    // 이차 탐사 : 충돌 방생 시 2^, 2^3 만큼 떨어진 빈 버킷을 찾아
+    // 값을 저장하는 방식 입니다.
+
+    // 이중 해싱 : 해시 값을 한법 더 해시 함수에 넣어 다른 함수를
+    // 도출하는 방식입니다.
+#pragma endregion
+
 
     return 0;
 }
--- DataStruct/HEAP.h
+++ DataStruct/HEAP.h
@@ -11,11 +11,26 @@ private:
 public:
 	void Insert(T data)
 	{
+		if (IsFull())
+		{
+			cout << "Heap is Full" << endl;
+			return;
+		}
 		arr[index] = data;
-
 		HeapSort(index);
-
 		index++;
+	}
+
+	bool IsFull()
+	{
+		if (index >= SIZE)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	void HeapSort(int index)
@@ -29,12 +44,46 @@ public:
 		{
 			swap(arr[index], arr[index / 2]);
 			HeapSort(index / 2);
-			
 		}
 		else
 		{
 			return;
 		}
+	}
+
+	T Delete()
+	{
+		if (index <= 0) // Heap이 비어있는 경우
+		{
+			cout << "Heap is Empty" << endl;
+			return -1;
+		}
+
+		T maxValue = arr[1];
+
+		arr[1] = arr[index];
+		arr[index] = NULL;
+		index--;
+
+		int parent = 1;
+		while (parent <= index)
+		{
+			if (arr[parent * 2] > arr[parent * 2 + 1])
+			{
+				swap(arr[parent], arr[parent * 2]);
+				parent = parent * 2;
+			}
+			else if(arr[parent * 2] < arr[parent * 2 + 1])
+			{
+				swap(arr[parent], arr[parent * 2 + 1]);
+				parent = parent * 2 + 1;
+			}
+			else
+			{
+				break;
+			}
+		}
+		return maxValue;
 	}
 
 	void Display()
